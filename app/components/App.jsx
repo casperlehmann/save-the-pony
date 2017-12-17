eb9532568c490a82c47f42e4a20d92557672f87d
Laspimon <casperlehmann@gmail.com>
2017-12-17 14:18:37 +0100

    TileTransformer, dynamic css, functional background assignment

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {contains_pony, contains_domo, contains_exit } from './aux'
 import {data, width, height, pony_pos, domo_pos, exit_pos} from './test_data'
 import {pony_character, domo_character, exit_character} from './resources'
 import {makeBoard} from './make_board'
-import {Tile} from './tile'
-import {can_walk_north, can_walk_east, can_walk_south, can_walk_west} from './navigation'
+import {Tile, TileTransformer} from './tile'
 
 export default class App extends React.Component {
   render(props) {
@@ -12,39 +10,43 @@ export default class App extends React.Component {
     const style = {
       width: '400px',
       height: '400px',
-
       display: 'grid',
+
+      borderTop: '1px',
+      borderBottom: '1px',
+      borderLeft: '1px',
+      borderRight: '1px',
+
+      borderStyle: 'solid',
+      borderColor: 'black',
+
       gridTemplateColumns: 'repeat(' + width + ', 1fr)',
       gridTemplateRows: 'repeat(' + height + ', 1fr)',
     };
+    const backgrounds = {}
+    backgrounds[pony_pos] = 'url('+pony_character+')';
+    backgrounds[domo_pos] = 'url('+domo_character+')';
+    backgrounds[exit_pos] = 'url('+exit_character+')';
+    backgrounds['default'] = 'url(https://i.imgur.com/c3EsnP6.jpg)';
+    function getBackground(position) {
+      return backgrounds[position] || backgrounds['default'];
+    }
+
     return (
       <div className="Board" style={style}>
         {tiles.map((row, rowIndex) => {
           return row.map((tileType, columnIndex) => {
-            const tile = tiles[rowIndex][columnIndex];
-            const pony = contains_pony(columnIndex, rowIndex, width, pony_pos);
-            const domo = contains_domo(columnIndex, rowIndex, width, domo_pos);
-            const exit = contains_exit(columnIndex, rowIndex, width, exit_pos);
+            const tile = TileTransformer(tiles, rowIndex, columnIndex, width, height);
             const index = rowIndex * width + columnIndex
-            var background
-            switch(true) {
-              case pony:
-                background = 'url('+pony_character+')'
-                break
-              case domo:
-                background = 'url('+domo_character+')'
-                break
-              case exit:
-                background = 'url('+exit_character+')'
-                break
-              'white'}
-            const north = can_walk_north(tile);
-            const east = can_walk_east(tile, row.length, columnIndex);
-            const south = can_walk_south(tile, tiles.length, rowIndex);
-            const west = can_walk_west(tile);
-            var clickHandler = (i) => alert('Clicked on: ' + i)
-            return <Tile north={north} south={south} east={east} west={west} background={background}
-              clickHandler={() => clickHandler(index)}/>;
+            const background = getBackground(index);
+            const clickHandler = (i) => alert('Clicked on: ' + i)
+            return <Tile
+              north = {tile.wallNorth}
+              south = {tile.wallSouth}
+              east = {tile.wallEast}
+              west = {tile.wallWest}
+              background = {background}
+              clickHandler = {() => clickHandler(index)}/>;
           })
         })}
       </div>
--- app/components/tile.jsx
+++ app/components/tile.jsx
@@ -8,12 +8,25 @@ export const Tile = (params) => {
       backgroundRepeat  : 'no-repeat',
       backgroundPosition: 'center',
   
-      borderLeft: params.west ? 2 : 0,
-      borderRight: params.east ? 2 : 0,
-      borderTop: params.north ? 2 : 0,
-      borderBottom: params.south ? 2 : 0,
+      borderLeft: params.west ? 1 : 0,
+      borderRight: params.east ? 1 : 0,
+      borderTop: params.north ? 1 : 0,
+      borderBottom: params.south ? 1 : 0,
       borderStyle: 'solid',
       borderColor: 'black',
     }
     return <div onClick={params.clickHandler} style={style}></div>;
   }
+
+export const TileTransformer = (tiles, rowIndex, columnIndex, width, height) => {
+    const current_tile = tiles[rowIndex][columnIndex];
+    const atEasternBorder = columnIndex + 1 == width;
+    const atSouthernBorder = rowIndex + 1 == height;
+
+    return {
+        wallNorth: current_tile.indexOf("north") >= 0,
+        wallWest: current_tile.indexOf("west") >= 0,
+        wallEast: atEasternBorder ? true : tiles[rowIndex][columnIndex+1].indexOf("west") >= 0,
+        wallSouth: atSouthernBorder ? true : tiles[rowIndex+1][columnIndex].indexOf("north") >= 0,
+    }
+}
