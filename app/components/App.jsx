083b016220dd1a7310a2e548d5bce430fe58b83a
Laspimon <casperlehmann@gmail.com>
2017-12-26 20:44:56 +0100

    Client site navigation rules

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,11 +3,49 @@ import {data, width, height, pony_pos, domo_pos, exit_pos} from './test_data'
 import {pony_character, domo_character, exit_character} from './resources'
 import {makeBoard} from './make_board'
 import {Tile, TileTransformer} from './tile'
+import PropTypes from 'prop-types';
 
 export default class App extends React.Component {
-  render(props) {
-    const tiles = makeBoard(data, width, height);
-    const style = {
+
+  constructor(props) {
+    super();
+    this.state = {
+      data: props.data,
+      width: props.width,
+      height: props.height,
+      pony_pos: props.pony_pos,
+      domo_pos: props.domo_pos,
+      exit_pos: props.exit_pos,
+      pony_paths: [],
+      gameStarted: false,
+    };
+  }
+
+  getBackground(position) {
+    if (this.state.pony_pos == position) {
+      return 'url('+pony_character+')'
+    }
+    else if (this.state.domo_pos == position) {
+      return 'url('+domo_character+')'
+    }
+    else if (this.state.exit_pos == position) {
+      return 'url('+exit_character+')'
+    }
+    else {
+      return 'url(https://i.imgur.com/c3EsnP6.jpg)'
+    }
+  }
+
+  clickHandler(i, thing) {
+    if (this.state.pony_paths.indexOf(i) != -1){
+    this.setState({pony_pos: i}, function() {
+      //console.log('pony click:', this.state.pony_pos)
+    });
+  }}
+
+  render() {
+    const tiles = makeBoard(this.state.data, this.state.width, this.state.height);
+    const outerStyle = {
       width: '400px',
       height: '400px',
       display: 'grid',
@@ -19,37 +57,62 @@ export default class App extends React.Component {
 
       borderStyle: 'solid',
       borderColor: 'black',
-
+    };
+    const gameStyle =  {
       gridTemplateColumns: 'repeat(' + width + ', 1fr)',
       gridTemplateRows: 'repeat(' + height + ', 1fr)',
     };
-    const backgrounds = {}
-    backgrounds[pony_pos] = 'url('+pony_character+')';
-    backgrounds[domo_pos] = 'url('+domo_character+')';
-    backgrounds[exit_pos] = 'url('+exit_character+')';
-    backgrounds['default'] = 'url(https://i.imgur.com/c3EsnP6.jpg)';
-    function getBackground(position) {
-      return backgrounds[position] || backgrounds['default'];
-    }
+    const menuStyle = {
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
+
+    if (false && !this.state.gameStarted) {return (
+      <div className="StartScreen" style={Object.assign(outerStyle, menuStyle)}>
+        <button
+          style={{width: '100%', height: '15%'}}
+          onClick={() => this.setState({gameStarted: true})}
+          >
+          Start Game</button>
+      </div>
+    )}
 
     return (
-      <div className="Board" style={style}>
+      <div className="Board" style={Object.assign(outerStyle, gameStyle)}>
         {tiles.map((row, rowIndex) => {
           return row.map((tileType, columnIndex) => {
             const tile = TileTransformer(tiles, rowIndex, columnIndex, width, height);
             const index = rowIndex * width + columnIndex
-            const background = getBackground(index);
-            const clickHandler = (i) => alert('Clicked on: ' + i)
+            if (this.state.pony_pos === index) {this.state.pony_paths = tile.walkable}
+            const background = this.getBackground(index);
             return <Tile
               north = {tile.wallNorth}
               south = {tile.wallSouth}
               east = {tile.wallEast}
               west = {tile.wallWest}
               background = {background}
-              clickHandler = {() => clickHandler(index)}/>;
+              clickHandler = {() => this.clickHandler(index)}/>;
           })
         })}
       </div>
     );
   }
+}
+
+App.propTypes = {
+  data: PropTypes.array,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  pony_pos: PropTypes.number,
+  domo_pos: PropTypes.number,
+  exit_pos: PropTypes.number,
+}
+
+App.defaultProps = {
+  data: data,
+  width: width,
+  height: height,
+  pony_pos: pony_pos,
+  domo_pos: domo_pos,
+  exit_pos: exit_pos,
 }
--- app/components/tile.jsx
+++ app/components/tile.jsx
@@ -22,11 +22,21 @@ export const TileTransformer = (tiles, rowIndex, columnIndex, width, height) => 
     const current_tile = tiles[rowIndex][columnIndex];
     const atEasternBorder = columnIndex + 1 == width;
     const atSouthernBorder = rowIndex + 1 == height;
+    const wallNorth = current_tile.indexOf("north") >= 0
+    const wallWest = current_tile.indexOf("west") >= 0
+    const wallEast = atEasternBorder ? true : tiles[rowIndex][columnIndex+1].indexOf("west") >= 0
+    const wallSouth = atSouthernBorder ? true : tiles[rowIndex+1][columnIndex].indexOf("north") >= 0
 
+    const walkablePaths = []
+    if (!wallNorth) {walkablePaths.push(columnIndex+rowIndex*width-width)}
+    if (!wallWest) {walkablePaths.push(columnIndex+rowIndex*width-1)}
+    if (!wallEast) {walkablePaths.push(columnIndex+rowIndex*width+1)}
+    if (!wallSouth) {walkablePaths.push(columnIndex+rowIndex*width+width)}
     return {
-        wallNorth: current_tile.indexOf("north") >= 0,
-        wallWest: current_tile.indexOf("west") >= 0,
-        wallEast: atEasternBorder ? true : tiles[rowIndex][columnIndex+1].indexOf("west") >= 0,
-        wallSouth: atSouthernBorder ? true : tiles[rowIndex+1][columnIndex].indexOf("north") >= 0,
+        wallNorth: wallNorth,
+        wallWest: wallWest,
+        wallEast: wallEast,
+        wallSouth: wallSouth,
+        walkable: walkablePaths
     }
 }
